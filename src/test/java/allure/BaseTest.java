94efc663ebec9160ea5e55e9731aeb4104d24083
andrewnnov <andrewnnov@yandex.ru>
2023-11-15 21:09:32 +0300

    add attach

--- src/test/java/allure/BaseTest.java
+++ src/test/java/allure/BaseTest.java
@@ -2,7 +2,9 @@ package allure;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -26,5 +28,14 @@ public class BaseTest {
         ));
 
         Configuration.browserCapabilities=capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last_screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 }
